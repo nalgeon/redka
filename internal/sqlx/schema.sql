c58976ae50a037cf2f7b2aa24d04418881afda1b
Anton <m@antonz.org>
2024-04-26 10:47:09 +0500

    refactor: sqlx - schema formatting

--- internal/sqlx/schema.sql
+++ internal/sqlx/schema.sql
@@ -3,12 +3,12 @@ pragma user_version = 1;
 -- keys
 create table if not exists
 rkey (
-    id       integer primary key,
-    key      text not null,
-    type     integer not null,
-	version  integer not null,
-    etime    integer,
-	mtime    integer not null
+  id       integer primary key,
+  key      text not null,
+  type     integer not null,
+  version  integer not null,
+  etime    integer,
+  mtime    integer not null
 );
 
 create unique index if not exists
@@ -24,17 +24,17 @@ before update of type on rkey
 for each row
 when old.type is not new.type
 begin
-    select raise(abort, 'key type mismatch');
+  select raise(abort, 'key type mismatch');
 end;
 
 -- strings
 create table if not exists
 rstring (
-    key_id integer not null,
-    value  blob not null,
+  key_id integer not null,
+  value  blob not null,
 
-    foreign key (key_id) references rkey (id)
-	  on delete cascade
+  foreign key (key_id) references rkey (id)
+	on delete cascade
 );
 
 create unique index if not exists
@@ -42,23 +42,23 @@ rstring_pk_idx on rstring (key_id);
 
 create view if not exists
 vstring as
-  select
-    rkey.id as key_id, rkey.key, rstring.value,
-	datetime(etime/1000, 'unixepoch') as etime,
-	datetime(mtime/1000, 'unixepoch') as mtime
-  from rkey join rstring on rkey.id = rstring.key_id
-  where rkey.type = 1
-    and (rkey.etime is null or rkey.etime > unixepoch('subsec'));
+select
+  rkey.id as key_id, rkey.key, rstring.value,
+  datetime(etime/1000, 'unixepoch') as etime,
+  datetime(mtime/1000, 'unixepoch') as mtime
+from rkey join rstring on rkey.id = rstring.key_id
+where rkey.type = 1
+  and (rkey.etime is null or rkey.etime > unixepoch('subsec'));
 
 -- hashes
 create table if not exists
 rhash (
-    key_id integer not null,
-    field text not null,
-    value blob not null,
+  key_id integer not null,
+  field text not null,
+  value blob not null,
 
-    foreign key (key_id) references rkey (id)
-      on delete cascade
+  foreign key (key_id) references rkey (id)
+    on delete cascade
 );
 
 create unique index if not exists
@@ -66,23 +66,23 @@ rhash_pk_idx on rhash (key_id, field);
 
 create view if not exists
 vhash as
-  select
-    rkey.id as key_id, rkey.key, rhash.field, rhash.value,
-	datetime(etime/1000, 'unixepoch') as etime,
-	datetime(mtime/1000, 'unixepoch') as mtime
-  from rkey join rhash on rkey.id = rhash.key_id
-  where rkey.type = 4
-    and (rkey.etime is null or rkey.etime > unixepoch('subsec'));
+select
+  rkey.id as key_id, rkey.key, rhash.field, rhash.value,
+  datetime(etime/1000, 'unixepoch') as etime,
+  datetime(mtime/1000, 'unixepoch') as mtime
+from rkey join rhash on rkey.id = rhash.key_id
+where rkey.type = 4
+  and (rkey.etime is null or rkey.etime > unixepoch('subsec'));
 
 -- sorted sets
 create table if not exists
 rzset (
-    key_id integer not null,
-    elem   blob not null,
-    score  real not null,
+  key_id integer not null,
+  elem   blob not null,
+  score  real not null,
 
-    foreign key (key_id) references rkey (id)
-      on delete cascade
+  foreign key (key_id) references rkey (id)
+    on delete cascade
 );
 
 create unique index if not exists
@@ -94,9 +94,9 @@ rzset_score_idx on rzset (key_id, score, elem);
 create view if not exists
 vzset as
 select
-    rkey.id as key_id, rkey.key, rzset.score, rzset.elem,
-	datetime(etime/1000, 'unixepoch') as etime,
-	datetime(mtime/1000, 'unixepoch') as mtime
+  rkey.id as key_id, rkey.key, rzset.elem, rzset.score,
+  datetime(etime/1000, 'unixepoch') as etime,
+  datetime(mtime/1000, 'unixepoch') as mtime
 from rkey join rzset on rkey.id = rzset.key_id
 where rkey.type = 5
-    and (rkey.etime is null or rkey.etime > unixepoch('subsec'));
+  and (rkey.etime is null or rkey.etime > unixepoch('subsec'));
