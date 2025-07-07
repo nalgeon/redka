ee6440c28a5cc390fe21593dbe2fab51d605c11e
Anton <m@antonz.org>
2025-07-07 06:34:10 +0500

    rkey: unified queries

--- internal/rkey/postgres.go
+++ internal/rkey/postgres.go
@@ -2,20 +2,8 @@ package rkey
 
 // Postgres queries for the key repository.
 var postgres = queries{
-	count: `
-	select count(id) from rkey
-	where key in (:keys) and (etime is null or etime > ?)`,
-
-	delete: `
-	delete from rkey
-	where key in (:keys) and (etime is null or etime > ?)`,
-
 	deleteAll: `
 	truncate table rkey cascade`,
-
-	deleteAllExpired: `
-	delete from rkey
-	where etime <= $1`,
 
 	deleteNExpired: `
 	delete from rkey
@@ -25,45 +13,9 @@ var postgres = queries{
 		limit $2
 	)`,
 
-	expire: `
-	update rkey set
-		version = version + 1,
-		etime = $1
-	where key = $2 and (etime is null or etime > $3)`,
-
-	get: `
-	select id, key, type, version, etime, mtime
-	from rkey
-	where key = $1 and (etime is null or etime > $2)`,
-
 	keys: `
 	select id, key, type, version, etime, mtime from rkey
 	where key like $1 and (etime is null or etime > $2)`,
-
-	len: `
-	select count(*) from rkey`,
-
-	persist: `
-	update rkey set
-		version = version + 1,
-		etime = null
-	where key = $1 and (etime is null or etime > $2)`,
-
-	random: `
-	select id, key, type, version, etime, mtime from rkey
-	where etime is null or etime > $1
-	order by random() limit 1`,
-
-	rename1: `
-	delete from rkey where id = $1`,
-
-	rename2: `
-	update rkey
-	set
-		key = $1,
-		version = version + 1,
-		mtime = $2
-	where key = $3 and (etime is null or etime > $4)`,
 
 	scan: `
 	select id, key, type, version, etime, mtime from rkey
@@ -72,4 +24,21 @@ var postgres = queries{
 		and (etime is null or etime > $5)
 	order by id asc
 	limit $6`,
+}
+
+func init() {
+	postgres.count = sqlite.count
+	postgres.delete = sqlite.delete
+	// postgres.deleteAll = sqlite.deleteAll
+	postgres.deleteAllExpired = sqlite.deleteAllExpired
+	// postgres.deleteNExpired = sqlite.deleteNExpired
+	postgres.expire = sqlite.expire
+	postgres.get = sqlite.get
+	// postgres.keys = sqlite.keys
+	postgres.len = sqlite.len
+	postgres.persist = sqlite.persist
+	postgres.random = sqlite.random
+	postgres.rename1 = sqlite.rename1
+	postgres.rename2 = sqlite.rename2
+	// postgres.scan = sqlite.scan
 }
